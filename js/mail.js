21992ff6b5c0d994a0b918db2ce2c116768388c1
mxcaamano95 <mxcaamano@outlook.com>
2022-05-14 20:52:44 -0300

    Validacion de datos en formulario OK

--- js/mail.js
+++ js/mail.js
@@ -1,21 +1,10 @@
 function ValidarInputs() {
-
-    var validChar = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  
-    if (document.getElementById('email').value.match(validChar)) {
-  
-      toastOK("Gracias! 👍 A la brevedad nos contactaremos!",'3000');
-  
-      return true;
-  
-    } else {
-  
-      toastError("Ingrese correctamente el mail");
-  
-      document.getElementById('email').focus();
-  
-      return false;
-  
-    }
-  
+    const nombre = document.getElementById('fname').value;
+    const apellido = document.getElementById('lname').value;
+    const email = document.getElementById('email').value;
+    const txtconsulta = document.getElementById('message').value;
+    const validChar = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    nombre !== "" && apellido !== "" && email !== "" && txtconsulta !== "" && email.match(validChar) ? 
+      toastOK(`Gracias! 👍 Nos contactaremos a la brevedad al correo: ${email}`,'3000') : toastError("Complete los campos vacíos");
+    email.match(validChar) ? "" : setTimeout(function(){ toastError("Recuerde que el eMail debe tener el siguiente formato: xxxxxxx@xxxxx.xxx ")}, 500);  
   }
